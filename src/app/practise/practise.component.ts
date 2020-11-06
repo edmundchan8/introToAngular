94da41b4c8d48d91a2d27537b826fbb94c3705a4
edmundchan8 <pokedev8@gmail.com>
2020-11-06 09:51:38 -0800

    latest push of my angular training

--- src/app/practise/practise.component.ts
+++ src/app/practise/practise.component.ts
@@ -3,23 +3,13 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-practise',
   templateUrl: './practise.component.html',
-  styleUrls: ['./practise.component.css']
+  styles: ['']
 })
 export class PractiseComponent implements OnInit {
-
-  public title = "Practising Angular"
-  public titleID = "titleID123"
-  public paragraphID = "paragraphID456"
-  public inputID = "inputID789"
-  // public inputID2 = "inputID7892"
-  // public inputID3 = "inputID7893"
-
-  public onDisabled = true;
-  public notDisabled = false;
+  public isDisplayed = true;
 
   constructor() { }
 
   ngOnInit(): void {
   }
-
 }
--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -2,36 +2,11 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-test',
-  template: 
-  `<div>
-    <h2 [class]=successText >Color is green</h2>
-    <h2 [class.text-danger]="hasError"> ERROR CODE!</h2>
-    <h2 [ngClass]="messageClasses" >OBJECT DIRECTIVE</h2>
-  </div>`,
-  styles: [
-    `
-    .text-success {
-      color: green;
-    }
-    .text-danger {
-      color: red;
-    }
-    .text-special {
-      font-style: italic;
-    }
-    `
-  ]
+  templateUrl: './test.component.html',
+  styleUrls: []
 })
 export class TestComponent implements OnInit {
-  
-  public hasError = true;
-  public isSpecial = true;
-  public messageClasses = {
-    "text-success": !this.hasError,
-    "text-danger": this.hasError,
-    "text-special": this.isSpecial
-  }
-  public successText = "text-success"
+
 
   constructor() { }
 
